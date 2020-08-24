75058dcf61eef92ee089b5c79804afd3ad751505
Anna Teng <anna.w.teng@gmail.com>
2020-08-24 16:30:48 -0400

    scraped the data

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,44 +1,54 @@
-const puppeteer = require('puppeteer');
+const axios = require('axios');
+const { JSDOM } = require('jsdom');
 const fs = require('fs');
+const path = require('path');
 const logger = require('./logger');
 
-const url = 'http://m3.mappler.net/dechr/web/index_list.php?blogid=nyrestroom&is_iframe_type_page=N&with_tabs=Y#loaded';
+const baseUrl = 'http://m3.mappler.net/dechr/class/list_data_web_fn.php?page=1&blogid=nyrestroom&is_blog_user_group_allow_yn=N&th_no_sort_class=sort_desc&th_name_sort_class=sort&page_per_record=100';
+const url = (n) => `http://m3.mappler.net/dechr/class/list_data_web_fn.php?page=1&blogid=nyrestroom&is_blog_user_group_allow_yn=N&th_no_sort_class=sort_desc&th_name_sort_class=sort&page_per_record=${n}`;
 
 (async () => {
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    await page.waitForSelector('select#page_per_record');
-    await page.select('select#page_per_record', '100');
-    await page.waitForResponse((res) => res.status() === 200, { timeout: 10000 });
-    // const rows = await page.$$eval('tr');
+    const data = [];
 
-    const data = await page.$$eval('tr', (rows) => {
-      const out = [];
+    // get number of entries
+    const { data: baseResData } = await axios.get(baseUrl);
+    const { document: baseDocument } = new JSDOM(baseResData).window;
+    const numEntries = baseDocument.querySelector('#data-table > tbody > tr:nth-child(1) > td:nth-child(1)').textContent;
 
-      rows.forEach((r, ind) => {
-        if (ind !== 0) { // first row is table headers
-          const tds = r.children;
-          const entry = {
-            index: tds.item(0).innerText,
-            name: tds.item(2).innerText,
-            category: tds.item(3).innerText,
-            address: tds.item(4).innerText,
-            hours: tds.item(5).innerText,
-            handicap: tds.item(6).innerText,
-            editor: tds.item(7).innerText,
-          };
+    // get full table of data
+    const { data: resData } = await axios.get(url(numEntries));
+    const { document } = new JSDOM(resData).window;
 
-          out.push(entry);
-        }
-      });
+    // parse data from rows
+    const rows = document.querySelectorAll('tr');
+    rows.forEach((r, ind) => {
+      if (ind !== 0) { // first row is table headers
+        const tds = r.children;
+        const entry = {
+          index: tds.item(0).textContent,
+          name: tds.item(2).textContent,
+          category: tds.item(3).textContent,
+          address: tds.item(4).textContent,
+          hours: tds.item(5).textContent,
+          handicap: tds.item(6).textContent,
+          editor: tds.item(7).textContent,
+        };
 
-      return out;
+        data.push(entry);
+      }
     });
-    logger.info(data);
 
-    await browser.close();
+    const jsonOutput = JSON.stringify(data);
+    fs.writeFile(
+      path.join(__dirname, '../data/mappler-data.json'),
+      jsonOutput,
+      { flag: 'w' },
+      (err) => {
+        if (err) throw err;
+        else logger.info('file written successfully');
+      },
+    );
   } catch (e) {
     logger.error(e);
   }
